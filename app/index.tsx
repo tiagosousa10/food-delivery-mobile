57ce36c8faf37617125b1247535a01f858142fff
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-12 13:57:08 +0100

    configure text fonts for the project

--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,8 @@ import "./globals.css";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+      <Text className="text-5xl text-center font-bold text-primary font-quicksand-bold">
+        Welcome to Food App!
       </Text>
     </View>
   );
