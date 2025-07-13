e5ea5dda71acccab16ba3a6a940fa3edb1693dfa
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-13 23:54:31 +0100

    create a flatlist to show my items

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,59 @@
-import { offers } from "@/constants";
-import { FlatList, Pressable, Text, View } from "react-native";
+import { images, offers } from "@/constants";
+import cn from "clsx";
+import { Fragment } from "react";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "./globals.css";
 
 export default function Index() {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1 bg-white">
       <FlatList
         data={offers}
         renderItem={({ item, index }) => {
+          const isEven = index % 2 === 0; // check if index is even
           return (
             <View>
-              <Pressable className="bg-amber-600 my-3 h-48 rounded-xl ">
-                <Text>{item.title}</Text>
+              <Pressable
+                className={cn(
+                  "offer-card",
+                  isEven ? "flex-row-reverse" : "flex-row"
+                )}
+                style={{ backgroundColor: item.color }}
+                android_ripple={{ color: "#fffff22" }}
+              >
+                {({ pressed }) => (
+                  <Fragment>
+                    <View className="h-full w-1/2">
+                      <Image
+                        source={item.image}
+                        className="size-full"
+                        resizeMode="contain"
+                      />
+                    </View>
+                    <View
+                      className={cn(
+                        "offer-card__info",
+                        isEven ? "pl-10" : "pr-10"
+                      )}
+                    >
+                      <Text className="h1-bold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className="size-10"
+                        resizeMode="contain"
+                        tintColor={"#ffffff"}
+                      />
+                    </View>
+                  </Fragment>
+                )}
               </Pressable>
             </View>
           );
         }}
+        contentContainerClassName="pb-28 px-5"
       />
     </SafeAreaView>
   );
