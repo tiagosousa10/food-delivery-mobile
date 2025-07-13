a53436d92894cf7c59063435f51c0f2f108afb4a
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-14 00:02:00 +0100

    create static header

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,23 @@
 import { images, offers } from "@/constants";
 import cn from "clsx";
 import { Fragment } from "react";
-import { FlatList, Image, Pressable, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "./globals.css";
 
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 bg-white">
+      {/* header */}
+
+      {/* items */}
       <FlatList
         data={offers}
         renderItem={({ item, index }) => {
@@ -54,6 +64,23 @@ export default function Index() {
           );
         }}
         contentContainerClassName="pb-28 px-5"
+        ListHeaderComponent={() => (
+          <View className="flex-between flex-row w-full my-5 px-5 ">
+            <View className="flex-start">
+              <Text className="small-bold text-primary">DELIVER TO</Text>
+              <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+                <Text className="paragraph-bold text-dark-100">Portugal</Text>
+                <Image
+                  source={images.arrowDown}
+                  className="size-3"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+
+            <Text>Cart</Text>
+          </View>
+        )}
       />
     </SafeAreaView>
   );
