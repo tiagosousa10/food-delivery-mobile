ad56484635089ba468600e7db8aec19a69e5bda9
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-16 17:44:50 +0100

    create user and save it in database

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,3 +1,7 @@
+import { Account, Avatars, Client, Databases, ID } from "react-native-appwrite";
+
+import { CreateUserParams, SignInParams } from "@/type";
+
 export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
@@ -5,3 +9,48 @@ export const appwriteConfig = {
   databaseId: "687545fa001b0d8f3dc6",
   userCollectionId: "6875462200015b48619a",
 };
+
+export const client = new Client();
+
+client
+  .setEndpoint(appwriteConfig.endpoint!)
+  .setProject(appwriteConfig.projectId!)
+  .setPlatform(appwriteConfig.platform);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+const avatars = new Avatars(client);
+
+export const createUser = async ({
+  email,
+  password,
+  name,
+}: CreateUserParams) => {
+  try {
+    const newAccount = await account.create(ID.unique(), email, password, name);
+
+    if (!newAccount) {
+      throw new Error("User not created");
+    }
+
+    await signIn({ email, password });
+
+    const avatarUrl = avatars.getInitialsURL(name);
+
+    return await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      {
+        accountId: newAccount.$id,
+        email,
+        name,
+        avatarUrl,
+      }
+    );
+  } catch (error) {
+    throw new Error(error as string);
+  }
+};
+
+export const signIn = async ({ email, password }: SignInParams) => {};
