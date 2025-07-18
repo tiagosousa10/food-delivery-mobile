265e20eb2840a503d761c5cf69681e6d81d083a4
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-18 17:54:56 +0100

    build search page, fetching data and first jsx for flatlist

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,52 @@
-import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { getCategories, getMenu } from "@/lib/appwrite";
+import useAppwrite from "@/lib/useAppwrite";
+import { useLocalSearchParams } from "expo-router";
+import React, { useEffect } from "react";
+import { FlatList, SafeAreaView, Text, View } from "react-native";
 
 const Search = () => {
+  const { category, query } = useLocalSearchParams<{
+    query: string;
+    category: string;
+  }>();
+
+  const { data, refetch, loading } = useAppwrite({
+    fn: getMenu,
+    params: { category, query, limit: 6 },
+  });
+  const { data: categories } = useAppwrite({ fn: getCategories });
+
+  useEffect(() => {
+    refetch({ category, query, limit: 6 });
+  }, [category, query]);
+
   return (
-    <SafeAreaView>
-      <Text>Search</Text>
+    <SafeAreaView className="bg-white h-full">
+      <FlatList
+        data={data}
+        renderItem={({ item, index }) => {
+          return (
+            <View className="flex-1 max-w-[48%]">
+              <Text>Menu Card</Text>
+            </View>
+          );
+        }}
+        keyExtractor={(item) => item.$id}
+        numColumns={2}
+        columnWrapperClassName="gap-7"
+        contentContainerClassName="gap-7 px-5 pb-32"
+        ListHeaderComponent={() => (
+          <View className="my-5 gap-5">
+            <View className="flex-between flex-row w-full">
+              <View className="flex-start">
+                <Text className="small-bold uppercase text-primary">
+                  Search
+                </Text>
+              </View>
+            </View>
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 };
