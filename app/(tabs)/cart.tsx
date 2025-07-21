2b7fac28c809dd9cfc2488545e2ae94a642eee34
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-21 12:16:01 +0100

    fetch items from cart store and jsx implementations for cart page

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -1,11 +1,54 @@
+import CustomHeader from "@/components/CustomHeader";
+import { useCartStore } from "@/store/cart.store";
+import { PaymentInfoStripeProps } from "@/type";
+import cn from "clsx";
 import React from "react";
-import { Text, View } from "react-native";
+import { FlatList, SafeAreaView, Text, View } from "react-native";
+
+const PaymentInfoStripe = ({
+  label,
+  value,
+  labelStyle,
+  valueStyle,
+}: PaymentInfoStripeProps) => (
+  <View className="flex-between flex-row my-1">
+    <Text className={cn("paragraph-medium text-gray-200", labelStyle)}>
+      {label}
+    </Text>
+    <Text className={cn("paragraph-bold text-dark-100", valueStyle)}>
+      {value}
+    </Text>
+  </View>
+);
 
 const Cart = () => {
+  const { items, getTotalItems, getTotalPrice } = useCartStore();
+
+  const totalItems = getTotalItems();
+  const totalPrice = getTotalPrice();
+
   return (
-    <View>
-      <Text>Cart</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <Text>Cart Item</Text>}
+        keyExtractor={(item) => item.id}
+        contentContainerClassName="pb-28 px-5 pt-5"
+        ListHeaderComponent={() => <CustomHeader title="Your Cart" />}
+        ListEmptyComponent={() => <Text>Cart Empty</Text>}
+        ListFooterComponent={() =>
+          totalItems > 0 && (
+            <View className="gap-5">
+              <View className="mt-6 border border-gray-200 p-5 rounded-2xl">
+                <Text className="h3-bold text-dark-100 mb-5">
+                  Payment Summary
+                </Text>
+              </View>
+            </View>
+          )
+        }
+      />
+    </SafeAreaView>
   );
 };
 
